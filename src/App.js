78f209ebb6bf90156d2e918025abdeb739355502
Vadim-Vatrya <VatryaVV@gmail.com>
2022-09-18 16:17:53 +0300

    add delete

--- src/App.js
+++ src/App.js
@@ -36,6 +36,15 @@ function App() {
     setCalc(eval(calc).toString());
   };
 
+  const calculateLast = () => {
+    if (calc === '') {
+      return;
+    }
+
+    const value = calc.slice(0, -1);
+    setCalc(value);
+  };
+
   return (
     <div className="App">
       <div className="calculator">
@@ -50,7 +59,7 @@ function App() {
           <button onClick={() => updateCalc('+')}> +</button>
           <button onClick={() => updateCalc('-')}> - </button>
 
-          <button>DEL</button>
+          <button onClick={calculateLast}>DEL</button>
         </div>
 
         <div className="digits">
